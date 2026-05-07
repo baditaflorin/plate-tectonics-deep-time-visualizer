e59fd59a05d21f8e1004b89a6e18cb8aec2d2fff
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-08 02:15:38 +0300

    chore: publish pages build

--- src/generated/buildInfo.ts
+++ src/generated/buildInfo.ts
@@ -1,6 +1,6 @@
 export const buildInfo = {
   "version": "0.1.0",
-  "commit": "a19b452",
+  "commit": "4f406bf",
   "branch": "main",
-  "builtAt": "2026-05-08T02:12:17+03:00"
+  "builtAt": "2026-05-08T02:15:02+03:00"
 } as const;
